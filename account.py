391a419e58882d30014c81e13e23009a3f1c351f
Mercy Wacera <waceraaamercy@gmail.com>
2022-06-09 20:30:25 +0300

    OOP

--- account.py
+++ account.py
@@ -1,35 +1,44 @@
 
-# Add these methods to class Account - deposit, withdraw. Create two instances of account and verify they work as expected.
-
 
 class Account:
-    
+   
+    withdraw=[]
     def __init__(self,name,accountnumber,pin):
         self.balance=0
         self. name= name
         self. accountnumber= accountnumber
-
         self.pin=pin
+        self.deposits=[]
+        self.withdraws=[]
 
      # class Method
     def deposit(self,deposit):
-        self.balance+= deposit
+
         if deposit <= 0:
             return f"Deposit amount should be more that 0"
         else:
+            self.balance+= deposit
+            self.deposits.append(self.balance)
             return f"you have deposited {deposit}, and this is your new balance {self.balance}" 
         
-
     def withdraw(self,withdraw):
-       if withdraw>self.balance:
+        if withdraw>self.balance:
            return f"your balance is {self.balance}, you cannot withdraw {withdraw}"
-       elif withdraw<0:
+        elif withdraw<0:
            return f"your amount should be more that zero"
-       else:
-           self.balance-= withdraw
-           return f"you have deposited {withdraw}, and this is your new balance {self.balance}" 
-        
-    
+        else:
+            self.balance-= withdraw
+            self.withdraws.append(self.balance)
+            return f"you have withdraw {withdraw}, and this is your new balance {self.balance}" 
+          
+    def deposit_statement(self):
+        print(*self.deposits, sep="\n")
+
+    def withdraw_statement(self):
+        print(*self.withdraws, sep="\n")
+
+
+ 
 
 
     
